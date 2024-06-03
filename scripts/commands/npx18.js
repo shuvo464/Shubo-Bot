735e766668ea0f7cb3916370c689c68dc7ef8968
MASUD-HRIDOYE404 <134224832+Masud-Hridoye404@users.noreply.github.com>
2024-06-03 20:10:26 +0600

    Update and rename Npxall.js to npx18.js

---
+++ scripts/commands/npx18.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+module.exports = {
+  config:{
+	name: "npx18",
+        version: "1.0.1",
+        prefix: false,
+	permssion: 0,
+	credits: "nayan", 
+	description: "Fun",
+	category: "no prefix",
+	usages: "😒",
+        cooldowns: 5, 
+},
+
+handleEvent: async function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+  const content = event.body ? event.body : '';
+  const body = content.toLowerCase();
+  const axios = require('axios')
+const media = (
+    await axios.get(
+      'https://drive.google.com/uc?id=1832NXQg_73wFNtY2gspe3jWnqJ7Lfo-m',
+      { responseType: 'stream' }
+    )
+  ).data;
+
+	if (body.indexOf("😁")==0 || body.indexOf("🥱")==0) {
+		var msg = {
+				body: "𝐌𝐃 𝐉𝐔𝐁𝐀𝐄𝐃 𝐀𝐇𝐌𝐌𝐄𝐃 𝐉𝐎𝐘",
+				attachment: media
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("💔", event.messageID, (err) => {}, true)
+		}
+	},
+	start: function({ nayan }) {
+  }
+} 
