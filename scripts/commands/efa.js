bff605b2d4d88ef2f0da4c8271e5009bd3dcc15e
MASUD-HRIDOYE404 <134224832+Masud-Hridoye404@users.noreply.github.com>
2024-07-10 22:04:28 +0600

    Create efa.js

---
+++ scripts/commands/efa.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "efa",
+  version: "2.0.0",
+  permission: 0,
+  credits: "nayan",
+  description: "",
+  prefix: false,
+  category: "user",
+  usages: "",
+  cooldowns: 5,
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("Joy")==0 || (event.body.indexOf("JOY")==0 || (event.body.indexOf("জয়")==0 || (event.body.indexOf("Joy")==0)))) {
+		var msg = {
+				body: "জয় বস এখন বিজি আছেন কি বলবেন আমাকে বলতে পারেন😻"
+    }
+			api.sendMessage(msg, threadID, messageID);
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+}
