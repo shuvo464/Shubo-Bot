0f96ba85e63b3c14c63bfa497e0066a155b0cbbd
joyefa566 <164298128+joyefa566@users.noreply.github.com>
2024-09-04 06:29:02 +0600

    Update prefix.js

--- scripts/commands/prefix.js
+++ scripts/commands/prefix.js
@@ -2,37 +2,51 @@ module.exports.config = {
   name: "prefix",
   version: "1.0.0",
   permission: 0,
-  credits: "ryuko",
+  credits: "Joy Ahmed",
   prefix: true,
   description: "guide",
   category: "system",
   usages: "",
   cooldowns: 5,
 };
-
-module.exports.handleEvent = async ({ event, api, Threads }) => {
-  var { threadID, messageID, body, senderID } = event;
-  function out(data) {
-    api.sendMessage(data, threadID, messageID)
-  }
-  var dataThread = (await Threads.getData(threadID));
-  var data = dataThread.data; 
-  const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
-
-  var arr = ["mpre","mprefix","prefix", "command mark", "What is the prefix of the bot?","PREFIX"];
-  arr.forEach(i => {
-    let str = i[0].toUpperCase() + i.slice(1);
-    if (body === i.toUpperCase() | body === i | str === body) {
-		const prefix = threadSetting.PREFIX || global.config.PREFIX;
-      if (config.PREFIX == null) {
-        return out(`bot prefix : ${global.config.PREFIX}`)
-      }
-      else return out(`bot prefix : ${global.config.PREFIX}`)
-    }
-
-  });
+module.exports.languages = {
+  "vi": {},
+  "en": {}
 };
 
-module.exports.run = async({ event, api }) => {
-    return api.sendMessage("no prefix commands", event.threadID)
-}
+function random(arr) {
+var rd = arr[Math.floor(Math.random() * arr.length)];
+    return rd;
+        };
+module.exports.handleEvent = async function ({ api, event, Threads }) {
+  const axios = require("axios")
+  const picture = (await axios.get(`https://i.imgur.com/TQeipJV.jpeg`, { responseType: "stream"})).data
+      const moment = require("moment-timezone");
+var gio = moment.tz("Asia/Dhaka").format("hh:mm:ss || D/MM/YYYY");
+  var thu =
+moment.tz('Asia/Dhaka').format('dddd');
+  if (thu == 'Sunday') thu = 'রবিবার'
+  if (thu == 'Monday') thu = 'সোমবার'
+  if (thu == 'Tuesday') thu = 'মঙ্গলবার'
+  if (thu == 'Wednesday') thu = 'বুধবার'
+  if (thu == "Thursday") thu = 'বৃহস্পতিবার'
+  if (thu == 'Friday') thu = 'শুক্রবার'
+  if (thu == 'Saturday') thu = 'শনিবার'
+  var { threadID, messageID, body } = event,{ PREFIX } = global.config;
+  let threadSetting = global.data.threadData.get(threadID) || {};
+  let prefix = threadSetting.PREFIX || PREFIX;
+  const icon = [""];
+  if (body.toLowerCase() == "prefix" || (body.toLowerCase() == "Prefix") ||  (body.toLowerCase() == "PREFIX") || (body.toLowerCase() == "PreFix")) {
+       api.sendMessage({body: `💐 ====『 𝗣𝗥𝗘𝗙𝗜𝗫 』==== 💐\n━━━━━━━━━━━━━━━━━━━\n[⏰] → 𝐃𝐚𝐭𝐚 𝐀𝐧𝐝 𝐓𝐢𝐦𝐞: ${gio} (${thu})\n[❤️] → 𝐁𝐨𝐭 𝐏𝐫𝐞𝐟𝐢𝐱: [ ${global.config.PREFIX} ]\n━━━━━━━━━━━━━━━━━━━\n[💥] → 𝐁𝐨𝐭 𝐡𝐚𝐬 𝐚 𝐩𝐨𝐬𝐬𝐢𝐛𝐥𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝 ${client.commands.size} \n[👥] → 𝐓𝐨𝐭𝐚𝐥 𝐁𝐨𝐭 𝐔𝐬𝐞𝐫𝐬: ${global.data.allUserID.length}\n[🏘️] → 𝐓𝐨𝐭𝐚𝐥 𝐆𝐫𝐨𝐮𝐩: ${global.data.allThreadID.length}\n━━━━━━━━━━━━━━━━━━━\n[👉] → 𝐓𝐡𝐢𝐬 𝐌𝐞𝐬𝐬𝐚𝐠𝐞 𝐓𝐨 𝐒𝐞𝐞 𝐂𝐨𝐦𝐦𝐨𝐧𝐥𝐲 𝐔𝐬𝐞𝐝 ${global.config.PREFIX} 𝐇𝐞𝐥𝐩.`, attachment: (await axios.get((await axios.get(`https://imran-api.onrender.com/video/crush`)).data.data, {
+                    responseType: 'stream'
+                })).data}, event.threadID, (err, info) => {
+    global.client.handleReaction.push({
+      name: this.config.name, 
+      messageID: info.messageID,
+      author: event.senderID,
+    })
+      },event.messageID);
+  }
+ }
+//ko api thì attachment: (picture)}, event.threadID, event.messageID);
+module.exports.run = async ({ api, event, args, Threads }) => {}
